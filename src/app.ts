05bc1a8dfd442986b1494eae2e56f5c65205acac
Adeniyi-Bella <adeniyibella@gmail.com>
2023-09-01 16:49:34 +0200

    feat(server): created a node js application with express

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import cors from "cors";
+import express, { Application } from "express";
+import ip from "ip";
+
+
+export class App {
+    private readonly app: Application;
+    private readonly APPLICATION_RUNNING = "application is running on:";
+    private readonly ROUTE_NOT_FOUND = "Route Does not exist on our server";
+
+    constructor(private readonly port: (string | number) = process.env.PORT || 3000) {
+        this.app = express();
+        this.middleWare();
+        this.routes();
+    }
+
+    listen(): void {
+        this.app.listen(this.port, () => {
+            console.log(`${this.APPLICATION_RUNNING} ${ip.address()}:${this.port}`);
+        });
+    }
+
+    private routes(): void {
+        this.app.use('/patients' , (req, res) => {});
+        this.app.get('/' , (req, res) => res.status(200).send({message: "Server is up and running"}));
+        //other routes
+        this.app.all('*' , (req, res) => res.status(404).send({message: this.ROUTE_NOT_FOUND}));
+
+    }
+
+    private middleWare(): void {
+        this.app.use(cors({ origin: "*"}));
+        this.app.use(express.json());
+    }
+
+}
